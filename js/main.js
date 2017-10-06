3e66e37a01ab5f286828c7ae9453284844fc0a5c
Lizák Gergő <lizak.gergo@zugloizrt.local>
2017-10-06 13:15:05 +0200

    create main.js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Callback függbény fade után
+function fadeDone() {
+    console.log(this);
+}
+
+// Eseménykezelő beállítása
+$("p").click(function(){
+    $(this).hide();
+    //$(this).fadeTo(5000, 1, fadeDone);
+    $(this).slideDown(3500);
+});
+
+// Esemény kiválasztása
+$("p").click();
+
+// Kattintás megelőzése
+$("nav a.nav-link").click( function(ev) {
+    ev.preventDefault();
+    var link = $(this);
+    $(document.body).animate({
+        opacity: '0'
+    }, 1000, function() {
+        document.location = link.attr("href");
+
+    })
+});
